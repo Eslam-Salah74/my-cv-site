8efc9f6d4aae7bfa16c07bce69d4e7b7bd5b3fb8
Eslam Salah <eslamsalah20003000@gmail.com>
2019-10-01 21:28:58 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>My-Cv-Site</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/fontawesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Bree+Serif|Orbitron|Patua+One|Prata&display=swap" rel="stylesheet">
+	<!--[if lt IE 9]-->
+      <script src="js/html5shiv.min.js"></script>
+      <script src="js/respond.min.js"></script>
+    <!--[endif]-->
+</head>
+<body>
+
+
+<!-- nave bar-->
+
+<header class="header navbar-fixed-top">
+	<div class="container">
+	 <nav class="navbar navbar-default">
+	  
+	    <!-- Brand and toggle get grouped for better mobile display -->
+	    <div class="navbar-header">
+	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+	        <span class="sr-only">Toggle navigation</span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </button>
+	      <a class="navbar-brand" href="#">Eslam.CV</a>
+	    </div>
+
+	    <!-- Collect the nav links, forms, and other content for toggling -->
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      
+	      <ul class="nav navbar-nav navbar-right">
+	      	<li><a href="about.php">About</a></li>
+	        <li><a href="index.php">Skilis</a></li>
+	        <li><a href="addpost.php">Services</a></li>
+	        
+	        <li><a href="index.php">Experience</a></li>
+	        <li><a href="addpost.php">Education</a></li>
+	        <li><a href="about.php">Portfolio</a></li>
+	        <li><a href="index.php">Testimonials</a></li>
+	        <li><a href="addpost.php">Contact</a></li>
+	        
+	      </ul>
+	    </div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
+</header>
+ <!-- end nave bar-->
+
+
+ <!-- start contant -->
+ <section class="content_container">
+ 	<div class="container">
+	 	<div class="row">
+
+	 		<div class="col-lg-4">
+	 			<div class="general_info">
+	 				<div class="background_image">
+	 					<img src="https://cdn.pixabay.com/photo/2017/06/28/04/29/adult-2449725_960_720.jpg" class="img-responsive" alt="Responsive image" height="400px" width="400px">
+	 				</div>
+	 				<div class="content_general_info">
+	 					<h2>General Information </h2>
+	 					<ul>
+	 						<li><i class="fa fa-home"> </i><span> Name:</span> Eslam Salah</li>
+	 						<li><i class="fa fa-home"> </i><span> Location:</span> Egypt,Giza</li>
+	 						<li><i class="fa fa-home"> </i><span> Date of Birth:</span> Octoper15,1996</li>
+	 						<li><i class="fa fa-home"> </i><span> Gimal:</span> eslamsalah20003000@gmail.com</li>
+	 						<li><i class="fa fa-home"> </i><span> Phon:</span> +2 01110731636</li>
+	 						<li><i class="fa fa-home"> </i><span> My Site:</span> www.mycv.com</li>
+	 					</ul>
+	 					<div class="social-media">
+	 						<i class="fa fa-home"></i>
+	 						<i class="fa fa-home"></i>
+	 						<i class="fa fa-home"></i>
+	 						<i class="fa fa-home"></i>
+	 					</div>
+	 				</div>
+	 			</div>
+	 			
+	 		</div>
+
+	 		<div class="col-lg-8">
+	 			<div class="min_content">
+	 				<div class="main_content_title">
+	 					<div class="header">
+	 						<div class="contant">
+		 					    <h3>HTML5 & CSS Developer</h3>
+		 					    <h1>Eslam Salah</h1>
+	 					    </div>
+	 					</div>
+	 					
+	 				</div>
+	 				<div class="main_content_scroll">
+	 					
+	 					<h1>Description</h1>
+		 				<p>
+		 				   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae sapien porttitor, dignissim quam sit amet, aliquam lorem. Fusce id ligula non risus mollis consectetur. Nam lobortis, erat quis pulvinar dignissim, velit ligula ullamcorper ipsum, at sodales elit odio at velit. Sed a est a quam mattis suscipit. Proin et faucibus diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae sapien porttitor, dignissim quam sit amet, aliquam lorem. Fusce id ligula non risus mollis consectetur. Nam lobortis, erat quis pulvinar dignissim, velit ligula ullamcorper ipsum, at sodales elit odio at velit. Sed a est a quam mattis suscipit.
+		 				   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae sapien porttitor, dignissim quam sit amet, aliquam lorem. Fusce id ligula non risus mollis consectetur. Nam lobortis, erat quis pulvinar dignissim, velit ligula ullamcorper ipsum, at sodales elit odio at velit. Sed a est a quam mattis suscipit. Proin et faucibus diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae sapien porttitor, dignissim quam sit amet, aliquam lorem. Fusce id ligula non risus mollis consectetur. Nam lobortis, erat quis pulvinar dignissim, velit ligula ullamcorper ipsum, at sodales elit odio at velit. Sed a est a quam mattis suscipit.
+		 				   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae sapien porttitor, dignissim quam sit amet, aliquam lorem. Fusce id ligula non risus mollis consectetur. Nam lobortis, erat quis pulvinar dignissim, velit ligula ullamcorper ipsum, at sodales elit odio at velit. Sed a est a quam mattis suscipit. Proin et faucibus diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae sapien porttitor, dignissim quam sit amet, aliquam lorem. Fusce id ligula non risus mollis consectetur. Nam lobortis, erat quis pulvinar dignissim, velit ligula ullamcorper ipsum, at sodales elit odio at velit. Sed a est a quam mattis suscipit.
+		 				</p>
+	 					
+	 				
+		 				<div class="visitor_counter">
+	 						<div class="col-lg-4">
+	 							<div class="contant">
+	 								<h2>250</h2>
+	 								
+	 							</div>
+	 							<h3>Total Visitors</h3>
+	 						</div>
+	 						<div class="col-lg-4">
+	 							<div class="contant">
+	 								<h2>250</h2>
+	 								
+	 							</div>
+	 							<h3>Total Visitors</h3>
+	 						</div>
+	 						<div class="col-lg-4">
+	 							<div class="contant">
+	 								<h2>250</h2>
+	 								
+	 							</div>
+	 							<h3>Total Visitors</h3>
+	 						</div>
+	 					</div>
+	 					
+	 					<div class="footer">
+	 						<div class="visitor_counter">
+	 						<div class="col-lg-6">
+	 							
+	 								<h5>The best educational courses</h5>
+	 								<ul>
+	 									<li>a</li>
+	 									<li>b</li>
+	 									<li>c</li>
+	 									<li>d</li>
+	 									<li>e</li>
+	 								</ul>
+	 								
+	 						
+	 							
+	 						</div>
+	 						<div class="col-lg-6">
+	 							
+	 								<h5>All My Projects</h5>
+	 								
+	 							<ul>
+	 									<li>a</li>
+	 									<li>b</li>
+	 									<li>c</li>
+	 									<li>d</li>
+	 									<li>e</li>
+	 								</ul>
+	 							
+	 						</div>
+	 						
+	 						</div>
+	 						<div class="copyright">
+	 							<p class="center">Copyright ©2019 All rights reserved | This template is made with  by</p>
+	 						</div>
+	 					</div>
+	 					</div>
+	 					
+	 					
+	 				</div>	
+
+
+	 					
+	 				</div>
+	 			</div>
+	 		</div>
+
+	 	</div>
+ 	</div>
+ </section>
+
+
+
+ <!-- end contant -->
+
+
+ <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+</html>	
